abed2749b0c0bf8133d3ec930c29bddba92e1c34
iPetlicki <petlicki.igor@gmail.com>
2024-10-06 23:54:13 +0200

    added server

--- vite.config.ts
+++ vite.config.ts
@@ -58,6 +58,10 @@ export default defineConfig({
     }),
     DefineOptions(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
